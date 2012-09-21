a7ff435e69fce81555b0a43e941174babb677e0a
Fabiano Beselga <fabianobeselga@gmail.com>
2012-09-20 22:56:11 -0300

    started the make me bad view

--- app.rb
+++ app.rb
@@ -78,6 +78,26 @@ post "/" do
   redirect "/"
 end
 
+get "/make_me_bad" do
+  # Get base API Connection
+  @graph  = Koala::Facebook::API.new(session[:access_token])
+
+  # Get public details of current application
+  @app  =  @graph.get_object(ENV["FACEBOOK_APP_ID"])
+
+  if session[:access_token]
+    @user    = @graph.get_object("me")
+    @friends = @graph.get_connections('me', 'friends')
+    @photos  = @graph.get_connections('me', 'photos')
+    @likes   = @graph.get_connections('me', 'likes').first(4)
+
+    # for other data you can always run fql
+    @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
+  end
+  @backgrounds = ['bb_wallpaper.jpeg', 'dark_knight.jpg']
+  erb :make_me_bad
+end
+
 # used to close the browser window opened to post to wall/send to friends
 get "/close" do
   "<body onload='window.close();'/>"
